85ea12bbf4fd3dbc9805e0a1696ba790d6bf5934
prateekahegde <88186454+prateekahegde@users.noreply.github.com>
2021-08-25 21:43:50 +0530

    Add files via upload

---
+++ day-6/login.php
@@ -0,0 +1,12 @@
+<?php
+
+$email = $_POST["email"];
+echo "Email - ";
+echo $email;
+echo "<br>";
+
+$passwd = $_POST["password"];
+echo "Password - ";
+echo $passwd;
+echo "<br>";
+?>
---
+++ day-6/register.php
@@ -0,0 +1,28 @@
+<?php
+
+$name = $_POST["name"];
+echo "Name - ";
+echo $name;
+echo "<br>";
+
+$email = $_POST["email"];
+echo "Email - ";
+echo $email;
+echo "<br>";
+
+$branch = $_POST["branch"];
+echo "Branch - ";
+echo $branch;
+echo "<br>";
+
+$clg = $_POST["clg"];
+echo "College - ";
+echo $clg;
+echo "<br>";
+
+$passwd = $_POST["passwd"];
+echo "Password - ";
+echo $passwd;
+echo "<br>";
+
+?>
